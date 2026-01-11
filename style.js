edcabe6ac11c1bbc5ad28dfbc62f1a7d4f6619bd
sakib3119 <sakibmozumder851@gamil.com>
2026-01-11 14:56:35 +0600

    added style.js

---
+++ style.js
@@ -0,0 +1,71 @@
+  let count = 0 ;
+  
+  function copy(){
+                count = count + 1;
+const  text =
+  document.getElementById('red').innerText;
+navigator.clipboard.writeText('999');
+   
+  document.getElementById('copy-count-11').innerText= count 
+  alert('নাম্বার টি successful copy করা হয়েছে ')
+  
+  } 
+
+
+
+  
+  function copp2(){
+                count = count + 1;
+const  text =
+  document.getElementById('red-1').innerText
+navigator.clipboard.writeText('1994-999999');
+   
+  document.getElementById('copy-count-11').innerText= count 
+  alert('নাম্বার টি successful copy করা হয়েছে ')
+  
+  } 
+
+
+  function copy3(){
+                count = count + 1;
+const  text =
+  
+navigator.clipboard.writeText('109');
+   
+  document.getElementById('copy-count-11').innerText= count 
+  alert('নাম্বার টি successful copy করা হয়েছে ')
+  
+  }
+
+   function copy4(){
+                count = count + 1;
+const  text =
+  
+navigator.clipboard.writeText('106');
+   
+  document.getElementById('copy-count-11').innerText= count 
+  alert('নাম্বার টি successful copy করা হয়েছে ')
+  
+  }
+
+    function copy5(){
+                count = count + 1;
+const  text =
+  
+navigator.clipboard.writeText('16216');
+   
+  document.getElementById('copy-count-11').innerText= count 
+  alert('নাম্বার টি successful copy করা হয়েছে ')
+  
+  }
+
+   function copy6(){
+                count = count + 1;
+const  text =
+  
+navigator.clipboard.writeText('16445');
+   
+  document.getElementById('copy-count-11').innerText= count 
+  alert('নাম্বার টি successful copy করা হয়েছে ')
+  
+  }
