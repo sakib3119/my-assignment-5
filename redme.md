c7149552a7081afb5925de0ac3b2fc607731c2d9
sakib3119 <sakibmozumder851@gamil.com>
2026-01-12 19:39:04 +0600

    added style.js

---
+++ redme.md
@@ -0,0 +1,18 @@
+What is the difference between getElementById, getElementsByClassName, and querySelector / querySelectorAll?
+
+
+ Ans = getElementById : নির্দিষ্ট একজনকে ধরার জন্য বা এটি সরাসরি ১টি এলিমেন্ট রিটার্ন করে।
+​getElementsByClassName: একই সাথে অনেকজনকে ধরার জন্য বা এটি একটি লিস্ট বা অ্যারে (Array) রিটার্ন করে 
+  querySelector: এটি পেজ থেকে কোনো নির্দিষ্ট আইডি বা ক্লাসের শুধুমাত্র প্রথম এলিমেন্টটিকে খুঁজে বের করে
+​querySelectorAll: এটি একই আইডি বা ক্লাসের সবগুলো এলিমেন্টকে একসাথে খুঁজে একটি লিস্ট  এ হিসেবে দেয়।
+
+
+
+How do you create and insert a new element into the DOM?
+
+Ans:  createElement দিয়ে নতুন ট্যাগ যেমন  div বা p তৈরি করে সেটির ভেতরে কন্টেন্ট দিতে হয়
+appendChild বা prepend ব্যবহার করে সেই ট্যাগটিকে HTML-এর কোনো নির্দিষ্ট বক্সের
+ভেতরে বসিয়ে দিতে হয়
+
+
+
--- style.js
+++ style.js
@@ -3,11 +3,10 @@
   function copy(){
                 count = count + 1;
 const  text =
-  document.getElementById('red').innerText;
-navigator.clipboard.writeText('999');
+  navigator.clipboard.writeText('999')
    
-  document.getElementById('copy-count-11').innerText= count 
-  alert('নাম্বার টি successful copy করা হয়েছে ')
+   document.getElementById('copy-count-11').innerText= count 
+  alert('নাম্বার টি successful copy করা হয়েছে')
   
   } 
 
@@ -69,3 +68,69 @@ navigator.clipboard.writeText('16445');
   alert('নাম্বার টি successful copy করা হয়েছে ')
   
   }
+
+  //  heart click count 
+    
+
+  
+function updadecount(){
+  count ++;
+  document.getElementById('heart-count').innerText = count;
+}
+// // call javascrif
+
+let coins = 100;
+
+function addCallLog() {
+    
+    
+    let now = new Date();
+    let time = now.toLocaleTimeString(); 
+
+
+
+
+
+    // coin
+   if (coins >= 20){
+      coins = coins - 20;
+      document.getElementById('coin-1').innerText = coins
+    
+     
+    
+    let historyBox = document.getElementById("history");
+
+    
+    let newLog = document.createElement("p");
+    newLog.innerHTML = "<strong>National Emergency</strong><br>999 -  " + time;
+    
+  
+
+  
+    historyBox.prepend(newLog);      
+   
+    alert("Calling National Emergency Service 999...");
+      }
+      else{
+        alert('আপনার পযাপ্ত কয়েন নেই। কল করার জন্য কমপক্ষে ২০ টা কয়েক লাগবে')
+      }
+  
+    
+    }
+
+
+
+    // clear button 
+
+    function clearHistory (){
+      let historyBox = document.getElementById('history');
+  if(historyBox){
+   historyBox.innerHTML=''
+     
+    
+    }
+
+   
+       
+    }
+     
